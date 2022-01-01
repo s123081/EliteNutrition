32e6e046916301bb8e0ca17d89bc96254d4daf32
Sinan Karer <sinan.karer@student.ap.be>
2022-01-01 15:21:44 +0300

    Artists page: opmaak en styling

--- src/pages/proteinbars/index.js
+++ src/pages/proteinbars/index.js
@@ -1,40 +1,133 @@
-import * as React from 'react'
-import { Link, graphql } from 'gatsby'
-import Layout from '../../components/layout'
+import * as React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Layout from "../../components/layout"
+import Proteinbar from "../../components/proteinbar"
+import {
+  hero,
+  section,
+  subtitle,
+  artists,
+  description,
+} from "../../page.module.css"
 
-// ...
 
-const ProteinBarsPage = ({data: {allWpProteinBar: {edges}}}) => {
+const ProteinBarsPage = ({
+  data: {
+    allWpProteinBar: { edges: proteinbarsInfo },
+    wpPage: { proteinBarsPage },
+  },
+}) => {
+  const image = getImage(proteinBarsPage.headerProteinbars.picture.localFile)
   return (
     <Layout pageTitle="Protein bars of Elite Agency">
-      {edges.map((item) => {
-        const proteinbar = item.node.proteinBarMeta;
-        const slug = item.node.slug;
-        return <Link to={`/proteinbars/${slug}`}>
-          <p>{proteinbar.name}</p>
-        </Link>
-
-      })}
+      <GatsbyImage
+      className={hero}
+        image={image}
+        alt={proteinBarsPage.headerProteinbars.picture.altText}
+      />
+      <div className={section}>
+        <h2 className={subtitle}>{proteinBarsPage.headerProteinbars.title}</h2>
+        <div
+        className={description}
+          dangerouslySetInnerHTML={{
+            __html: proteinBarsPage.headerProteinbars.description,
+          }}
+        />
+        <div className={artists}>
+          {proteinbarsInfo.map(({ node: proteinbar }) => (
+            <Proteinbar key={proteinbar.id} slug={proteinbar.slug} proteinbar={proteinbar} />
+          ))}
+        </div>
+      </div>
     </Layout>
   )
 }
 
 export const query = graphql`
 query {
-  allWpProteinBar {
-    edges {
-      node {
-        proteinBarMeta{
-          name
+  wpPage(slug: {eq: "proteinbars"}) {
+    proteinBarsPage {
+      headerProteinbars {
+        description
+        title
+        picture {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(quality: 100, placeholder: BLURRED, layout: FULL_WIDTH)
+            }
+          }
+          altText
         }
-        id
-        slug
       }
     }
   }
+
+allWpProteinBar {
+  edges {
+    node {
+      proteinBarMeta {
+        name
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, transformOptions: {grayscale: true})
+            }
+          }
+          altText
+        }
+      }
+      slug
+      id
+    }
+  }
+}
 }
 
 `
 
+/*
+query MyQuery {
+  wpPage(slug: {eq: "proteinbars"}) {
+    proteinBarsPage {
+      headerProteinbars {
+        description
+        title
+        picture {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(quality: 100, placeholder: BLURRED, layout: FULL_WIDTH)
+            }
+          }
+          altText
+        }
+      }
+    }
+  }
+}
+*/
 
+/*
+query MyQuery {
+  allWpProteinBar {
+    edges {
+      node {
+        proteinBarMeta {
+          name
+          image {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(placeholder: BLURRED, transformOptions: {grayscale: true})
+              }
+            }
+            altText
+          }
+        }
+        slug
+        id
+      }
+    }
+  }
+}
+*/
 export default ProteinBarsPage
