01253dc68f15e39a9dcd57971594e552a7610295
Sinan Karer <sinan.karer@student.ap.be>
2022-01-05 13:11:30 +0300

    Artist template page: opmaak en styling

--- src/pages/proteinbars/{wpProteinBar.slug}.js
+++ src/pages/proteinbars/{wpProteinBar.slug}.js
@@ -1,30 +1,64 @@
-import * as React from 'react'
-import { graphql } from 'gatsby'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image' 
-import Layout from '../../components/layout'
+import * as React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Layout from "../../components/layout"
+import {
+  header,
+  headerInfo,
+  headerPicture,
+  artistName,
+  fullName,
+  artistRoles,
+  artistDescription,
+  artistInfo,
+  artistPictures,
+  artistPicture,
+} from "../../page.module.css"
 
-// ...
+
 
 const ArtistPage = ({
   data: {
-    wpProteinBar: {proteinBarMeta: proteinbar},
+    wpProteinBar: {
+      proteinBarMeta: proteinbar,
+      diets: { nodes: diets},
+    },
   },
  }) => {
   const image = getImage(proteinbar.image.localFile)
+  const picture1 = getImage(proteinbar.pictures.picture1.localFile)
+  const picture2 = getImage(proteinbar.pictures.picture2.localFile)
+  const picture3 = getImage(proteinbar.pictures.picture3.localFile)
+
   return (
     <Layout pageTitle="Artiesten Template">
-    <div>
-      <GatsbyImage image={image} alt={proteinbar.image.altText}/>
-      <h3>{proteinbar.name}</h3>
-      <h1>{proteinbar.name}</h1>
-      <div dangerouslySetInnerHTML={{__html: proteinbar.description}} />
-      <p>Weight: {proteinbar.weight}</p>
-      <p>Flavour: {proteinbar.flavour}</p>
-      <p>Grams Of Protein: {proteinbar.gramOfProtein}</p>
-      <p>Calories: {proteinbar.calories}</p>
-      <p>Ingredients: <br></br> {proteinbar.ingredients}</p>
-      <p>Allergy: {proteinbar.allergy}</p>
+    <div className={header}>
+        <div className={headerInfo}>
+          {proteinbar.name && <h3 className={artistName}>{proteinbar.name}</h3>}
+          <div className={artistRoles}>
+            {diets.map((diet, i) => (
+              <span key={i}>
+                {diet.name} {i + 1 < diets.length && "- "}
+              </span>
+            ))}
+          </div>
+          
+          <div className={artistDescription} dangerouslySetInnerHTML={{__html: proteinbar.description}} />
+      <p><span className={artistInfo}>Weight: </span>{proteinbar.weight} </p>
+      <p><span className={artistInfo}>Flavour: </span>{proteinbar.flavour}</p>
+      <p><span className={artistInfo}>Grams Of Protein: </span>{proteinbar.gramsOfProtein}</p>
+      <p><span className={artistInfo}>Calories: </span>{proteinbar.calories}</p>
+      <p><span className={artistInfo}>Allergy: </span>{proteinbar.allergy}</p>
+      <p><span className={artistInfo}>Ingredients: </span>{proteinbar.ingredients}</p>
+
     </div>
+        <GatsbyImage className={headerPicture} image={image} alt={proteinbar.image.altText} />
+      </div>
+      <div className={artistPictures}>
+        <GatsbyImage className={artistPicture} image={picture1} alt={proteinbar .pictures.picture1.altText} />
+        <GatsbyImage className={artistPicture} image={picture2} alt={proteinbar.pictures.picture2.altText} />
+        <GatsbyImage className={artistPicture} image={picture3} alt={proteinbar.pictures.picture3.altText} />
+      </div>
     </Layout>
   )
 }
@@ -34,26 +68,55 @@ query ($id: String) {
   wpProteinBar(id: {eq: $id}) {
     proteinBarMeta {
       name
-      weight
       flavour
       calories
       gramsOfProtein
+      weight
       ingredients
       allergy
       description
       image {
-        localFile{
-				childImageSharp {
+        localFile {
+          childImageSharp {
             gatsbyImageData(placeholder: BLURRED)
           }
         }
         altText
       }
+      pictures {
+        picture1 {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+        }
+        picture2 {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+        }
+        picture3 {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+        }
+      }
+    }
+    diets {
+      nodes {
+        name
+      }
     }
   }
 }
-
-
 `
 
 export default ArtistPage
